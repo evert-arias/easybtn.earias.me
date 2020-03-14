9a48e85956490cf17437bcae45a2d4b4ff9837c4
Evert Arias <evert@codefor.me>
2020-03-14 01:17:35 -0400

    Update sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,8 @@
 module.exports = {
   someSidebar: {
-    EasyButton: ['introduction', 'installation']
+    'Getting started': ['introduction', 'installation'],
+    'API':['api-onPressed', 'api-onPressedFor', 'api-onSequence'],
+    'Examples':['example-on-pressed', 'example-on-pressed-for-duration', 'example-on-sequence'],
+    'Guides':['guide-getting-started-with-easybutton'],
   },
 };
