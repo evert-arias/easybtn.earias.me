82bedb2e6ed0e58e0541a83fd46fee8f90894958
Evert Arias <evert@codefor.me>
2020-03-16 01:35:50 -0400

    Update sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,8 @@
 module.exports = {
   someSidebar: {
     'Getting started': ['introduction', 'installation', 'fundamentals'],
-    'API':['api-on-pressed', 'api-on-pressed-for', 'api-on-sequence'],
-    'Examples':['basic-example', 'pressed-for-example', 'sequence-example'],
+    'API':['single-press-api', 'pressed-for-api', 'sequence-api'],
+    'Examples':['single-press-example', 'pressed-for-duration-example', 'sequence-example'],
     'Guides':['guide-getting-started-with-easybutton']
   }
 };
