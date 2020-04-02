a6364dc9dd7e5c71cf7c53f989fa12bb58abc5a2
Evert Arias <evert.arias@hotmail.com>
2020-04-01 21:23:39 -0400

    Udpate Contributor Component

--- src/components/Contributors/index.jsx
+++ src/components/Contributors/index.jsx
@@ -2,17 +2,53 @@ import React from "react";
 import ContributorProfile from "../ContributorProfile";
 import styles from "./styles.module.css";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { Octokit } from "@octokit/rest";
+let octokit = new Octokit();
 
-const Contributors = () => {
-  const context = useDocusaurusContext();
-  const { siteConfig = {} } = context;
-  return (
-    <div className={styles.contributors}>
-      {siteConfig.customFields.contributors.map(user => {
-        return <ContributorProfile username={user} />;
-      })}
-    </div>
-  );
-};
+class Contributors extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contributors: []
+    };
+  }
+
+  componentDidMount() {
+    octokit.repos
+      .listContributors({
+        owner: this.props.owner,
+        repo: this.props.repo
+      })
+      .then(
+        result => {
+          this.setState({
+            contributors: [...result.data]
+          });
+        },
+        error => {
+          this.setState({
+            contributors: []
+          });
+        }
+      );
+  }
+  render() {
+    return (
+      <div className={styles.contributors}>
+        {this.state.contributors && this.state.contributors.length
+          ? this.state.contributors.map(contributor => {
+              return (
+                <ContributorProfile
+                  avatar={contributor.avatar_url}
+                  name={contributor.login}
+                  url={contributor.html_url}
+                />
+              );
+            })
+          : undefined}
+      </div>
+    );
+  }
+}
 
 export default Contributors;
